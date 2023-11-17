a4ca3887d091da58ab7e36f221633e34596a05dc
Ayash13 <m.ayashal.f@gmail.com>
2023-11-17 15:02:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ucp_2/data/DataClass.kt
@@ -0,0 +1,10 @@
+package com.example.ucp_2.data
+
+data class DataClass(
+    val nama : String = "",
+    val nim : String = "",
+    val konsentrasi : String = "",
+    val judul : String = "",
+    val Dosbing1 : String = "",
+    val Dosbing2: String = ""
+)
---
+++ app/src/main/java/com/example/ucp_2/data/DataSource.kt
@@ -0,0 +1,16 @@
+package com.example.ucp_2.data
+
+import com.example.ucp_2.R
+
+object DataSource {
+    val Dosbing1 = listOf(
+        R.string.haris,
+        R.string.joko,
+        R.string.aprilia
+    )
+    val Dosbing2 = listOf (
+        R.string.haris,
+        R.string.joko,
+        R.string.aprilia
+    )
+}
